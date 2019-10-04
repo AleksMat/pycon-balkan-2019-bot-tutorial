fcf1c3c40895e67997522cc808a4f259745343b9
AleksMat <matej.aleksandrov@sinergise.com>
2019-10-04 11:22:32 +0200

    added all

---
+++ github_bot/__main__.py
@@ -0,0 +1,9 @@
+from octomachinery.app.server.runner import run as run_app
+
+
+if __name__ == "__main__":
+    run_app(
+        name='pycon-balkan-aleksmat-bot',
+        version='0.0.1',
+        url='https://github.com/settings/apps/pycon-balkan-aleksmat-bot',
+    )
